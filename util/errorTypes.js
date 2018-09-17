8c1052c411610e98c3703849dda8efd786244517
wuhxxx <wuhxxx@gmail.com>
2018-09-16 20:29:26 -0700

    error types

--- util/errorTypes.js
+++ util/errorTypes.js
@@ -1,16 +1,29 @@
 // Define and expose all error types in this project
 
-// Error has a http code and name and message(has default)
+// Error has name, http code and message(has default)
 
 // error builder
-const builder = (name, code, message) => {
-    const err = new Error(message);
-    err.name = name;
-    err.code = code;
-    return err;
+const builder = (name, code, defaultMessage) => {
+    return (message = defaultMessage) => {
+        const err = new Error(message);
+        err.name = name;
+        err.code = code;
+        return err;
+    };
 };
 
 // Expose errors
+// each error has a default message, also accepts custom message
 module.exports = {
+    // user errors
+    EmailRegistered: builder(
+        "EmailRegistered",
+        409,
+        "Email address is already registered"
+    ),
+    UserNotFound: builder("UserNotFound", 404, "User not found"),
+    WrongPassword: builder("WrongPassword", 400, "Incorrect password"),
+
+    // event errors
     EventNotFound: builder("EventNotFound", 404, "Event not found")
 };
