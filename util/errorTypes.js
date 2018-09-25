0d22bcc56953f25956c7914b602d67eb80f8bc85
wuhxxx <wuhxxx@gmail.com>
2018-09-25 03:52:00 -0700

    Rewrite to offer a more intuitive usage

--- util/errorTypes.js
+++ util/errorTypes.js
@@ -1,49 +1,60 @@
 // Define and expose all error types in this project
 
-// Error has name, http code and message(has default)
-
 /**
- * The error type builder, each error type has name, http code, default message
- * and a member function to retireve corresponding Error object
+ * A custom error type builder, returns custom error classes.
+ * Each error type has a type name, a http status code and a default message
  *
  * @param {String} name              Error's name
- * @param {Number} code              Error's HTTP status code
+ * @param {Number} statusCode        Error's HTTP status code
  * @param {String} defaultMessage    Error's default message
  *
- * @return {Object} An error type with name, code and default error message
+ * @return {Class} An error class, which inherits the built-in Error class
  */
-const buildErrorType = (name, code, defaultMessage) => {
-    // Error type object
-    const errorType = { name, code, defaultMessage };
+const ErrorType = (typeName, statusCode, defaultMessage) => {
+    // The custom error class, accepts optional custom message
+    class CustomError extends Error {
+        constructor(message = defaultMessage) {
+            super(message);
+            this.name = typeName;
+            this.statusCode = statusCode;
+            // clip the constructor invocation from the stack trace.
+            // see Nodejs's doc: https://nodejs.org/api/errors.html
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+    // assign properties to CustomError class,
+    // so that other modules can easily reference information about error types
+    CustomError.typeName = typeName;
+    CustomError.statusCode = statusCode;
+    CustomError.defaultMessage = defaultMessage;
 
-    /**
-     * Function to retrieve corresponding Error object
-     * @param {String} [message=default]  Error message
-     *
-     * @return {Error} Error object
-     */
-    errorType.getError = (message = defaultMessage) => {
-        const err = new Error(message);
-        err.name = name;
-        err.code = code;
-        return err;
-    };
-
-    return errorType;
+    return CustomError;
 };
 
-// Expose errors
-// each error has a default message, also accepts custom message
+/** Usage Example **/
+// const NotFound = ErrorType("NotFound", 404, "Resource not found");
+// console.log(NotFound.typeName); // NotFound
+// console.log(NotFound.statusCode); // 404
+// console.log(NotFound.defaultMessage); // Resource not found
+// const err = new NotFound("User not found");
+// console.log(typeof err); // object
+// console.log(err instanceof Error); // true
+// console.log(err.name); // NotFound
+// console.log(err.statusCode); // 404
+// console.log(err.message); // User not found
+// throw new NotFound(); // stack trace ends here
+
+// Define and expose errors
 module.exports = {
     // user errors
-    EmailRegistered: buildErrorType(
+    EmailRegistered: ErrorType(
         "EmailRegistered",
         409,
         "Email address is already registered"
     ),
-    UserNotFound: buildErrorType("UserNotFound", 404, "User not found"),
-    WrongPassword: buildErrorType("WrongPassword", 400, "Incorrect password"),
+    UserNotFound: ErrorType("UserNotFound", 404, "User not found"),
+    WrongPassword: ErrorType("WrongPassword", 400, "Incorrect password"),
 
     // event errors
-    EventNotFound: buildErrorType("EventNotFound", 404, "Event not found")
+    EventNotFound: ErrorType("EventNotFound", 404, "Event not found")
 };
