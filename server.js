6f59ade43c2ac7dd0748a1de443ae872f9443c33
wuhxxx <wuhxxx@gmail.com>
2019-01-13 16:27:50 -0800

    extend CORS allow header

--- server.js
+++ server.js
@@ -47,10 +47,7 @@ app.use(auth.initialize());
 // enable cross-origin resource sharing (CORS)
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    res.header("Access-Control-Allow-Headers", "*");
     res.header(
         "Access-Control-Allow-Methods",
         "POST, GET, DELETE, PATCH, OPTIONS"
